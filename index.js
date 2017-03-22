464969d80d7a017a1b196e53f0422c0d4ae86a76
astelvida <sevda.m.anefi@gmail.com>
2017-03-22 04:48:59 -0700

    added stdin methods to read input

--- index.js
+++ index.js
@@ -1,44 +1,95 @@
+
+// process.stdin.resume();
+// process.stdin.setEncoding('utf8');
+// var util = require('util');
+//
+// process.stdin.on('data', function (text) {
+//   console.log('received data:', util.inspect(text));
+//   if (text === 'quit\n') {
+//     done();
+//   }
+// });
+//
+// function done() {
+//   console.log('Now that process.stdin is paused, there is nothing more to do.');
+//   process.exit();
+// }
+
+
 const util = require('util');
 
+
 class TicTacToe {
-  constructor(board = [], round = 'x') {
+  constructor(board = [], round = 'X', moves = 0) {
     this.board = board;
     this.round = round;
+    this.moves = moves;
   }
 
-  startGame() {
-    this.makeBoard();
-    this.getRound();
+
+  checkGameEnd() {
+    if(this.moves === 9) {
+      return 'DONE';
+    }
+  }
+
+  playTurn(player) {
+    // check if game is done setWinner
+    if(this.moves === 3) {
+      return 'WINNER';
+    }
+    // intialize board
+    if(!this.board.length) {
+      this.makeBoard();
+    }
     this.printBoard();
+    process.stdout.write('*** \x1b[33m'+ player + '\x1b[0m' + " it's your turn!\n")
+    process.stdout.write('*** Enter coordinates:');
+    this.handleInput(player)
   }
 
-  setRound() {
-    this.round === 'x'? 'o': 'x';
+
+
+
+  handleInput(player) {
+    process.stdin.resume();
+    return new Promise((res, rej) => {
+      process.stdin.on('data', (buffer) => {
+        const input = buffer.toString().replace(/\s/g, '');
+        if(!input.length || input[0] >= 3 || input[1] >= 3
+          || this.board[input[0]][input[1]] !== '?') {
+          rej(input);
+        } else {
+          res(input);
+        }
+      });
+    })
+    .then((input) => {
+      this.board[input[0]][input[1]] = player;
+      this.moves++;
+      const nextRound = player === 'X'? 'O': 'X';
+      this.playTurn(nextRound);
+    })
+    .catch((err) => {
+      process.stdout.write('*** Try again! Please enter valid coordinates: ');
+      this.handleInput(player);
+    })
+
   }
 
-  getRound() {
-    process.stdout.write('\n'+ this.round + " it's your turn!\n\n");
-    return this.round;
-  }
-
-  movePlayer() {
-    const x = process.argv[2];
-    const y = process.argv[3];
-    this.board[x][y] = this.getRound();
-    this.printBoard();
-    this.setRound();
-  }
 
   makeBoard() {
     for(var i = 0; i < 3; i++) {
       this.board[i] = [];
       for(var j = 0; j < 3; j++) {
-        this.board[i][j] = 0;
+        this.board[i][j] = '?';
       }
     }
   }
 
+
   printBoard() {
+    process.stdout.write('\n\n\x1b[36m********************************************\n\n')
     process.stdout.write('\x1b[33m    0   1   2\n')
     process.stdout.write('\x1b[0m   --- --- ---\n')
     for(var i = 0; i < 3; i++) {
@@ -53,7 +104,5 @@ class TicTacToe {
 }
 
 var game = new TicTacToe;
-game.startGame();
-game.movePlayer();
-
+game.playTurn('X');
 module.exports = TicTacToe;
