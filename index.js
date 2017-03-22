d1da0d930ae76d1b395c733ed158ff1c53099e35
astelvida <sevda.m.anefi@gmail.com>
2017-03-22 03:05:16 -0700

    added colors inside console

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const util = require('util');
+
 class TicTacToe {
   constructor(board = [], round = 'x') {
     this.board = board;
@@ -15,12 +17,15 @@ class TicTacToe {
   }
 
   getRound() {
-    console.log(this.round + " it's your turn!");
+    process.stdout.write('\n'+ this.round + " it's your turn!\n\n");
     return this.round;
   }
 
-  movePlayer(x, y) {
+  movePlayer() {
+    const x = process.argv[2];
+    const y = process.argv[3];
     this.board[x][y] = this.getRound();
+    this.printBoard();
     this.setRound();
   }
 
@@ -34,20 +39,21 @@ class TicTacToe {
   }
 
   printBoard() {
-    process.stdout.write(" --- --- ---\n")
+    process.stdout.write('\x1b[33m    0   1   2\n')
+    process.stdout.write('\x1b[0m   --- --- ---\n')
     for(var i = 0; i < 3; i++) {
-      process.stdout.write("| ")
+      process.stdout.write('\x1b[33m'+(i)+'\x1b[0m'+ " | ")
       for(var j = 0; j < 3; j++) {
         process.stdout.write(this.board[i][j] + " | ");
       }
-      process.stdout.write("\n --- --- ---")
+      process.stdout.write("\n   --- --- ---")
       process.stdout.write("\n")
     }
   }
 }
 
 var game = new TicTacToe;
-
 game.startGame();
+game.movePlayer();
 
 module.exports = TicTacToe;
